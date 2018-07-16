f19649a45c41ec0345dd920983a91c7c75f20423
Mark Aduol <mark.aduol15@imperial.ac.uk>
2018-07-16 22:22:39 +0100

    WIP: Ran simple share-portal startup command; no immediate errors.

---
+++ config.js
@@ -0,0 +1,3 @@
+let config = {};
+config.debug = true;
+module.exports = config;
---
+++ index.js
@@ -0,0 +1,6 @@
+const PapyrusPackage = require('./lib/papyrus-package');
+module.exports = new PapyrusPackage({
+  workspace: atom.workspace,
+  notificationManager: atom.notifications,
+  clipboard: atom.clipboard
+});
