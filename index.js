2bf6587fab0ba356b4bf0478f2684483dbd6d080
Mark Aduol <mark.aduol15@imperial.ac.uk>
2018-07-28 01:03:44 +0100

    WIP: Working on implementation of GuestPortalBinding and full end-to-end state machine for joining a portal. - Added CSS files for UI - Worked on View Components for UI

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const PapyrusPackage = require('./lib/papyrus-package');
+const PortalBindingManager = require('./lib/client/portal-binding-manager.js');
 module.exports = new PapyrusPackage({
   workspace: atom.workspace,
   notificationManager: atom.notifications,
-  clipboard: atom.clipboard
+  clipboard: atom.clipboard,
+  portalBindingManager: new PortalBindingManager({
+    workspace: atom.workspace,
+    notificationManager: atom.notifications
+  })
 });
