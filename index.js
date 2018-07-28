5690aa9380fe9020a5c47fe1cca41a86fb2a33f4
Mark Aduol <mark.aduol15@imperial.ac.uk>
2018-07-28 17:18:36 +0100

    WIP: Fixing bugs and working on UI

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ module.exports = new PapyrusPackage({
   workspace: atom.workspace,
   notificationManager: atom.notifications,
   clipboard: atom.clipboard,
+  tooltipManager: atom.tooltips,
   portalBindingManager: new PortalBindingManager({
     workspace: atom.workspace,
     notificationManager: atom.notifications
