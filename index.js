f03da396fc60dcdac259d26817bfe865646e26eb
Mark Aduol <mark.aduol15@imperial.ac.uk>
2018-08-20 00:40:18 +0100

    Added missing files.

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 const PapyrusPackage = require('./lib/papyrus-package');
-const PortalBindingManager = require('./lib/client/portal-binding-manager.js');
 module.exports = new PapyrusPackage({
   workspace: atom.workspace,
   notificationManager: atom.notifications,
   clipboard: atom.clipboard,
   tooltipManager: atom.tooltips,
   commandRegistry: atom.commands,
-  portalBindingManager: new PortalBindingManager({
-    workspace: atom.workspace,
-    notificationManager: atom.notifications
-  })
 });
